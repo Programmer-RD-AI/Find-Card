faef5ac3db66d5a345e560b2aa22618221ddcbbc
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-10-11 10:23:30 +0530

    Add files via upload

--- 0.py
+++ 0.py
@@ -1,3 +1,4 @@
+# Imports
 import numpy as np
 import os, cv2, torch, torchvision
 import detectron2
@@ -12,18 +13,20 @@ import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import wandb
+from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 
+# Version
 print(torch.__version__, torchvision.__version__)
 data = pd.read_csv("./Data.csv")
 idx = 0
 
-
+# Loading Data
 def load_data(data=data, test=False):
     if test is True:
-        if "test_data.npy" in os.listdir("./"):
-            data = np.load("./test_data.npy", allow_pickle=True)
+        if "data.npy" in os.listdir("./"):
+            data = np.load("./data.npy", allow_pickle=True)[:625]
             return data
-        data = data[:1250]
     if "data.npy" in os.listdir("./"):
         data = np.load("./data.npy", allow_pickle=True)
         return data
@@ -54,59 +57,66 @@ def load_data(data=data, test=False):
         record["xmax"] = xmax
         record["ymax"] = ymax
         new_data.append(record)
-    if test is True:
-        np.save("test_data.npy", new_data)
-    else:
-        np.save("data.npy", new_data)
+    np.save("data.npy", new_data)
     return new_data
 
 
+# Config
 labels = ["Card"]
 
+# Adding the data
 DatasetCatalog.register("data", lambda: load_data())
 MetadataCatalog.get("data").set(thing_classes=labels)
 metadata = MetadataCatalog.get("data")
 DatasetCatalog.register("test", lambda: load_data(test=True))
 MetadataCatalog.get("test").set(thing_classes=labels)
 metadata_test = MetadataCatalog.get("test")
-wandb.init(sync_tensorboard=True, name="baseline")
+
 torch.cuda.empty_cache()
+wandb.init(sync_tensorboard=True, name="baseline")
 model = "COCO-Detection/faster_rcnn_R_101_DC5_3x.yaml"
+# Create the model
 cfg = get_cfg()
 cfg.merge_from_file(get_config_file(model))
 cfg.DATASETS.TRAIN = ("data",)
-cfg.DATASETS.TEST = ("test",)
+cfg.DATASETS.TEST = ()
 cfg.MODEL.WEIGHTS = get_checkpoint_url(model)
 cfg.SOLVER.MAX_ITER = 500
-# cfg.TEST.EVAL_PERIOD = 50
+cfg.TEST.EVAL_PERIOD = 50
 cfg.SOLVER.BASE_LR = 0.00025
 cfg.SOLVER.STEPS = []
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(labels)
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.25
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+# Create trainer
 trainer = DefaultTrainer(cfg)
+wandb.log(trainer.test(cfg, model))
+trainer.test(cfg, model)
 trainer.resume_or_load(resume=False)
 trainer.train()
-# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.475
-cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-img = cv2.imread("./Img/1.png")
-# predictor = DefaultPredictor(cfg)
-# preds = predictor(img)["instances"].to("cpu")
-# v = Visualizer(img[:, :, ::-1], metadata=metadata, scale=1)
-# v = v.draw_instance_predictions(preds)
-# v = v.get_image()[:, :, ::-1]
-# plt.figure(figsize=(12, 6))
-# plt.imshow(v)
-# plt.savefig("./img-0.png")
-# plt.close()
+# Eval The Model
+evaluator = COCOEvaluator("test", output_dir="./output")
+val_loader = build_detection_test_loader(cfg, "test")
+print(inference_on_dataset(trainer.model, val_loader, evaluator))
+wandb.log(trainer.test(cfg, model))
+trainer.test(cfg, model)
+evaluator = COCOEvaluator("data", output_dir="./output")
+val_loader = build_detection_test_loader(cfg, "data")
+print(inference_on_dataset(trainer.model, val_loader, evaluator))
+# Predict
 for img in os.listdir("./test_imgs/"):
     predictor = DefaultPredictor(cfg)
     img = cv2.imread(f"./test_imgs/{img}")
-    v = Visualizer(img[:, :, ::-1], metadata=metadata)
+    v = Visualizer(
+        img[:, :, ::-1],
+        metadata=metadata,
+        scale=0.5,
+    )
     v = v.draw_instance_predictions(predictor(img)["instances"].to("cpu"))
     plt.figure(figsize=(10, 7))
     plt.imshow(v.get_image()[:, :, ::-1])
-    plt.savefig(f"./preds/{img}")
-    plt.close()
-    # wandb.log({f"preds/{img}": wandb.Image(cv2.imread(f"./preds/{img}"))})
+    plt.show()
+    wandb.log({f"Img/{img}": wandb.Image(v.get_image()[:, :, ::-1])})
 wandb.finish()
