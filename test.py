08b571466a394123427697a89cbe243a7656a655
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-10-28 17:22:20 +0530

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from torchmetrics import MeanSquaredError
-import torch
-target = torch.tensor([[2.5, 5.0, 4.0, 8.0],[2.5, 5.0, 4.0, 8.0]])
-preds = torch.tensor([[3.0, 5.0, 2.5, 7.0],[3.0, 5.0, 2.5, 7.0]])
-mean_squared_error = MeanSquaredError()
-print(mean_squared_error(preds, target))
