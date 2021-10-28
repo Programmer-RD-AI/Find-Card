2e67efeffad2308e952041b6ead99a4e5aa674b8
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-10-28 17:29:35 +0530

    Add files via upload

---
+++ V1/test/test4.py
@@ -0,0 +1,9 @@
+import cv2
+img = cv2.imread(f"./Imgs/0.jpeg")  # get the original image
+img = cv2.resize(img, (1080, 1080))
+y = 100  # Cropping y
+x = 100  # Cropping x
+h = 900  # Cropping h
+w = 900  # Cropping w
+crop_img = img[y : y + h, x : x + w]  # Cropping
+cv2.imwrite(f"./Preds/cropped.jpeg", crop_img)  # Saving the corped imag
