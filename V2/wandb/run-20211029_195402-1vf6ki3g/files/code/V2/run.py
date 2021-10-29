5d4f55d4c68b3eeffcc664f9b0c92e8fa3a77be9
Programmer-RD-AI <aigithub@gmail.com>
2021-10-29 19:57:03 +0530

    Update

--- V2/wandb/run-20211029_195402-1vf6ki3g/files/code/V2/run.py
+++ V2/wandb/run-20211029_195402-1vf6ki3g/files/code/V2/run.py
@@ -20,21 +20,30 @@ params = {
         "faster_rcnn_R_101_FPN_3x.yaml",
         "faster_rcnn_X_101_32x8d_FPN_3x.yaml",
     ],
+    "LABELS": [["Card"]],
+    "MAX_ITER": [500],
     "BASE_LR": [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001],
     "IMS_PER_BATCH": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     "BATCH_SIZE_PER_IMAGE": [8, 16, 32, 64, 128, 256, 512],
+    "EVAL_PERIOD": [250],
+    "SCORE_THRESH_TEST": [0.625],
+    "CREATE_TARGET_AND_PREDS": [59],
 }
 params = ParameterGrid(params)
 params_iter = tqdm(params)
 for param in params_iter:
-    print(param)
     params_iter.set_description(str(param))
     model = Model(
+        base_lr=param["BASE_LR"],
+        labels=param["LABELS"],
+        max_iter=param["MAX_ITER"],
+        eval_period=param["EVAL_PERIOD"],
         ims_per_batch=param["IMS_PER_BATCH"],
         batch_size_per_image=param["BATCH_SIZE_PER_IMAGE"],
+        score_thresh_test=param["SCORE_THRESH_TEST"],
         model="COCO-Detection/" + param["MODEL"],
-        base_lr=param['BASE_LR'],
         name=str(param),
+        create_target_and_preds=param["CREATE_TARGET_AND_PREDS"],
     )
     metrics = model.train()
 # models = [
