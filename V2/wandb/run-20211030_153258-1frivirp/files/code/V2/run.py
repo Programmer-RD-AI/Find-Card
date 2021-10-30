6d7bedcb780f277cedcf91f0c6c0f7b6d6526504
Programmer-RD-AI <aigithub@gmail.com>
2021-10-30 15:58:21 +0530

    Update

--- V2/wandb/run-20211030_153258-1frivirp/files/code/V2/run.py
+++ V2/wandb/run-20211030_153258-1frivirp/files/code/V2/run.py
@@ -27,27 +27,16 @@ params = {
 params = ParameterGrid(params)
 params_iter = tqdm(params)
 for param in params_iter:
-    try:
-        torch.cuda.empty_cache()
-        model.remove_files_in_output()
-        torch.cuda.empty_cache()
-        params_iter.set_description(str(param))
-        torch.cuda.empty_cache()
-        model = Model(
-            ims_per_batch=param["IMS_PER_BATCH"],
-            batch_size_per_image=param["BATCH_SIZE_PER_IMAGE"],
-            model="COCO-Detection/" + param["MODEL"],
-            base_lr=param["BASE_LR"],
-            name=str(param),
-        )
-        torch.cuda.empty_cache()
-        metrics = model.train()
-        torch.cuda.empty_cache()
-    except Exception as e:
-        print("*" * 50)
-        print(e)
-        print("*" * 50)
-        torch.cuda.empty_cache()
+    model.remove_files_in_output()
+    params_iter.set_description(str(param))
+    model = Model(
+        ims_per_batch=param["IMS_PER_BATCH"],
+        batch_size_per_image=param["BATCH_SIZE_PER_IMAGE"],
+        model="COCO-Detection/" + param["MODEL"],
+        base_lr=param["BASE_LR"],
+        name=str(param),
+    )
+    metrics = model.train()
 # models = [
 #     # "fast_rcnn_R_50_FPN_1x.yaml",
 #     "faster_rcnn_R_50_C4_1x.yaml",
