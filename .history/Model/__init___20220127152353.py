409a95bb9100d55609ed888665d124788b349831
Programmer-RD-AI <aigithub@gmail.com>
2022-01-27 16:06:34 +0530

    Update
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Model/__init___20220127152353.py
@@ -0,0 +1,25 @@
+# Imports
+try:
+    from ray import tune
+except:
+    tune = None
+import urllib.request
+
+import matplotlib.pyplot as plt
+
+try:
+    pass
+except Exception as e:
+    raise ImportError(
+        f"""
+        Cannot Import Tqdm try installing it using 
+        `pip3 install tqdm` 
+        or 
+        `conda install tqdm`.
+        \n 
+        {e}"""
+    )
+from Model.help_funcs import *
+from Model.metrics import *
+from Model.modelling.detectron2 import *
+from Model.datasets import *
